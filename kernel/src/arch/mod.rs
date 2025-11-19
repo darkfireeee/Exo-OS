7374812bdd18f18a4b126b718e3ed9ed5e19d42b
darkfireeee <xavierlife@gmail.com>
2025-11-19 20:39:13 +0000

    initial commit

--- kernel/src/arch/mod.rs
+++
@@ -1,8 +0,0 @@
-// src/arch/mod.rs
-// Abstraction d'architecture - Point d'entrée pour l'architecture spécifique
-
-#[cfg(target_arch = "x86_64")]
-pub use self::x86_64::*;
-
-#[cfg(target_arch = "x86_64")]
-pub mod x86_64;
--- kernel/src/libutils/arch/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Abstractions spécifiques à l'architecture
-//! 
-//! Ce module fournit des abstractions pour les fonctionnalités spécifiques
-//! à l'architecture matérielle.
-
-pub mod x86_64;
-
-// Réexportations
-pub use x86_64::{registers, interrupts};
